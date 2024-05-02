398516b5a7c081be92017bdfb26b62afca630b3d
Ryûga <corentingambier@gmail.com>
2024-05-02 14:43:02 +0200

    Correction lien cliquable dans index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $films = $resultat->fetchAll(PDO::FETCH_CLASS);
 echo"<h1 class='title'>Liste des meilleurs Films des années 2010 : </h1>";
 
 foreach($films as $film) {
-    echo "<li class='list-item'><a href='film.php?id=" . $film->id . "'>" . $film->titre . " (" . $film->libelle . " de " . $film->prenom . " " . $film->nom . ", " . $film->annee . ")</a></li>";}
+    echo "<li class='list-item'><a href='film.php?id=" . $film->id . "'>" . $film->titre . " </a>(" . $film->libelle . " de " . $film->prenom . " " . $film->nom . ", " . $film->annee . ")</li>";}
 ?>
 
 </body>
