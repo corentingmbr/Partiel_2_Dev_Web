7a4e5109eafc2d0f817f2488529daf4b26cd669d
Ryûga <corentingambier@gmail.com>
2024-05-02 13:29:49 +0200

    Update partiel2.sql

--- partiel2.sql
+++ partiel2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 02, 2024 at 11:14 AM
+-- Generation Time: May 02, 2024 at 11:29 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -31,11 +31,22 @@ CREATE TABLE `film` (
   `id` int(10) NOT NULL,
   `duree` int(10) NOT NULL,
   `titre` varchar(50) NOT NULL,
-  `description` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `date de sortie` date NOT NULL,
   `genre` int(10) NOT NULL,
   `realisateur` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id`, `duree`, `titre`, `description`, `date de sortie`, `genre`, `realisateur`) VALUES
+(1, 169, 'Interstellar', 'Le film raconte les aventures d’un groupe d’explorateurs qui utilisent une faille récemment découverte dans l’espace-temps afin de repousser les limites humaines et partir à la conquête des distances astronomiques dans un voyage interstellaire. ', '2014-11-05', 1, 1),
+(2, 164, 'Django Unchained', 'Dans le sud des États-Unis, deux ans avant la guerre de Sécession, le Dr King Schultz, un chasseur de primes allemand, fait l’acquisition de Django, un esclave qui peut l’aider à traquer les frères Brittle, les meurtriers qu’il recherche. Schultz promet à Django de lui rendre sa liberté lorsqu’il aura capturé les Brittle – morts ou vifs.', '2013-01-16', 2, 2),
+(3, 148, 'Inception', 'Dom Cobb est un voleur expérimenté – le meilleur qui soit dans l’art périlleux de l’extraction : sa spécialité consiste à s’approprier les secrets les plus précieux d’un individu, enfouis au plus profond de son subconscient, pendant qu’il rêve et que son esprit est particulièrement vulnérable. Très recherché pour ses talents dans l’univers trouble de l’espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier qui a perdu tout ce qui lui est cher.', '2010-07-21', 1, 1),
+(4, 105, 'Coco', 'Depuis déjà plusieurs générations, la musique est bannie dans la famille de Miguel. Un vrai déchirement pour le jeune garçon dont le rêve ultime est de devenir un musicien aussi accompli que son idole, Ernesto de la Cruz.\r\n\r\nBien décidé à prouver son talent, Miguel, par un étrange concours de circonstances, se retrouve propulsé dans un endroit aussi étonnant que coloré : le Pays des Morts. Là, il se lie d’amitié avec Hector, un gentil garçon mais un peu filou sur les bords.', '2017-11-15', 3, 3),
+(5, 122, 'Joker', 'Le film, qui relate une histoire originale inédite sur grand écran, se focalise sur la figure emblématique de l’ennemi juré de Batman. Il brosse le portrait d’Arthur Fleck, un homme sans concession méprisé par la société. ', '2019-10-09', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -47,6 +58,16 @@ CREATE TABLE `genre` (
   `id` int(10) NOT NULL,
   `libelle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`id`, `libelle`) VALUES
+(1, 'Science Fiction'),
+(2, 'Western'),
+(3, 'Animation'),
+(4, 'Drame');
 
 -- --------------------------------------------------------
 
@@ -60,6 +81,16 @@ CREATE TABLE `realisateur` (
   `prenom` varchar(50) NOT NULL,
   `nationnalite` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `realisateur`
+--
+
+INSERT INTO `realisateur` (`id`, `nom`, `prenom`, `nationnalite`) VALUES
+(1, 'Nolan', 'Christopher', 'Americain'),
+(2, 'Tarantino', 'Quentin', 'Americain'),
+(3, 'Molina ', 'Adrian', 'Americain'),
+(4, 'Phillips', 'Todd', 'Americain');
 
 --
 -- Indexes for dumped tables
@@ -93,19 +124,19 @@ ALTER TABLE `realisateur`
 -- AUTO_INCREMENT for table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `realisateur`
 --
 ALTER TABLE `realisateur`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
